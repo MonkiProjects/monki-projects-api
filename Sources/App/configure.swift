20fb93e2a76e81769079264b962454a5d4b9b67a
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-01-08 00:24:19 +0100

    Use in-memory SQLite for tests

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,6 +8,7 @@
 
 import Fluent
 import FluentPostgresDriver
+import FluentSQLiteDriver
 import Vapor
 
 // configures your application
@@ -15,16 +16,16 @@ public func configure(_ app: Application) throws {
 	// uncomment to serve files from /Public folder
 	// app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 	
-	app.databases.use(.postgres(
-		hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-		port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-		username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-		password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-		database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-	), as: .psql)
-	
 	if app.environment == .testing {
-		try app.autoRevert().wait()
+		app.databases.use(.sqlite(.memory), as: .sqlite)
+	} else {
+		app.databases.use(.postgres(
+			hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+			port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+			username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+			password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+			database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+		), as: .psql)
 	}
 	
 	app.migrations.add(User.Migrations.all)
