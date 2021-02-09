2f33bf16624448fadf7e98eed39f52fe6da5bcb1
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-02-09 23:29:37 +0100

    Add MonkiMapModel dependency

--- Package.swift
+++ Package.swift
@@ -13,6 +13,11 @@ let package = Package(
 		.package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
 		.package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
 		.package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+		.package(
+			name: "monki-map-model",
+			url: "https://github.com/MonkiProjects/monki-map-model-swift.git",
+			.branch("main")
+		),
 	],
 	targets: [
 		.target(
@@ -21,6 +26,7 @@ let package = Package(
 				.product(name: "Fluent", package: "fluent"),
 				.product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
 				.product(name: "Vapor", package: "vapor"),
+				.product(name: "MonkiMapModel", package: "monki-map-model"),
 			],
 			resources: [
 				.process("Resources"),
